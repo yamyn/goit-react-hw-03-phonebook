55ef96b0315b92d4ac7abccbaf294e6cbd4375f4
ya_myn <yaroslav.mynchenko@onix-systems.com>
2020-05-09 14:17:42 +0300

    Updates

---
+++ precache-manifest.f29c242fe8e2c5b0cc494f0a288680f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc8add96eb4d69324c1ce2a8a61dc1a",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "7a2c62307608d356ac02",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.fd94e982.chunk.css"
+  },
+  {
+    "revision": "5d2566951ad0c8c0a1d9",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.70ab9f54.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.70ab9f54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a2c62307608d356ac02",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.3b525636.chunk.js"
+  },
+  {
+    "revision": "653c99886214260df0f5",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.067fb2b4.js"
+  }
+]);
